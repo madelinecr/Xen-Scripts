aa865f5091ecd77164217679e4dcc765e81e32db
Blaine Pace <blainepace@gmail.com>
2010-03-01 14:18:46 -0800

    Script now creates new folder and copies image file.

--- newguest.pl
+++ newguest.pl
@@ -2,32 +2,67 @@
 use strict;
 use warnings;
 
+use File::Copy;
+
 # Global variables
-my $imagedir = "/Users/Sensae/Desktop/vserver/images";
-my $xendir = "/Users/Sensae/Desktop/vserver/domains";
+my $imagedir = "/Users/Sensae/Desktop/vserver/images/";
+my $xendir = "/Users/Sensae/Desktop/vserver/domains/";
 
-print "\nPlease enter a hostname for your new guest: ";
 
-my $hostname = <>;
-
-print "\nReading contents of the image directory: \n";
-&ReadDir;
-print "\nPlease make a selection for your new VM: ";
-my $imageselection = <>;
-
-sub ReadDir
+# Get a hostname
+my $hostname;
+while(1)
 {
-	opendir(my $dirhandle, $imagedir) || die;
-
-	# do not read . or ..
-	my @dircontents = grep(/^.+\..+$/, readdir($dirhandle));
-
-	my $count = 1;
-	foreach(@dircontents)
+	print "\nPlease enter a hostname for your new guest: ";
+	$hostname = <>;
+	if($hostname =~ /^[a-zA-Z0-9]+$/)
 	{
-		print $count, " ", $_, "\n";
-		$count++;
+		chop $hostname;
+		last;
 	}
-
-	print $dircontents[0];
+	else
+	{
+		print "Sorry, hostname requires a-z, A-Z, 0-9 no spaces. \n";
+	}
 }
+
+#Read image directory
+print "\nReading contents of the image directory: \n";
+opendir(my $dirhandle, $imagedir) || die "Can't open directory";
+
+# do not read . or ..
+my @dircontents = grep(/^.+\..+$/, readdir($dirhandle));
+
+my $count = 1;
+foreach(@dircontents)
+{
+	print $count, " ", $_, "\n";
+	$count++;
+}
+
+#Get which image is wanted
+my $imageselection;
+while(1)
+{	
+	print "\nPlease make a selection for your new VM: ";
+	$imageselection = <>;
+	if($imageselection =~ /^[0-9]+$/)
+	{
+		chop $imageselection;
+		last;
+	}
+	else
+	{
+		print "Sorry, not a number.";
+	}
+}
+
+#Make a new domain folder and copy over image
+print "\nCreating new domain folder.\n";
+mkdir $xendir . $hostname || die "Can't create new domain folder";
+
+my $fromdir = $imagedir . $dircontents[$imageselection - 1];
+my $todir = $xendir . $hostname . "/disk.img";
+print "Copying from:\n", $fromdir . "\nTo:\n", $todir . "\n";
+
+copy($fromdir, $todir) || die "Couldn't copy image file.";
